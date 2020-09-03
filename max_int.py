26b6de44d21ef321c790dd755ddf4c9bf3bd6076
sigmar snær <sigmarsnaer@gmail.com>
2020-09-03 13:36:02 +0000

    bætti við forritinu sjalfu

---
+++ max_int.py
@@ -0,0 +1,15 @@
+# tekur input og ber það saman við öll previous input
+# á meðan input er 0 eða meira þá heldur forritið áfram
+# 
+
+num_int = int(input("Input a number: ")) 
+max_int = 0
+
+while num_int > 0:
+    if num_int >= max_int:
+        num_int == max_int
+num_int = int(input("Input a number: ")) 
+
+
+
+print("The maximum is", max_int) 
